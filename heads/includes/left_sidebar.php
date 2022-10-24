d469c2eb056c4d2e2c9302695ce3471fb30d463f
Sandeep Chandra Maddipati <82793936+SandeepChandraMaddipati@users.noreply.github.com>
2022-10-24 22:55:41 +0530

    Add files via upload

---
+++ heads/includes/left_sidebar.php
@@ -0,0 +1,71 @@
+<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="../vendors/images/deskapp-logo-svg.png" alt="" class="dark-logo">
+				<img src="../vendors/images/deskapp-logo-white-svg.png" alt="" class="light-logo">
+			</a>
+			<div class="close-sidebar" data-toggle="left-sidebar-close">
+				<i class="ion-close-round"></i>
+			</div>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li class="dropdown">
+						<a href="index.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-house-1"></span><span class="mtext">Dashboard</span>
+						</a>
+						
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-library"></span><span class="mtext">Staff</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add_staff.php">New Staff</a></li>
+							<li><a href="staff.php">Manage Staff</a></li>
+						</ul>
+					</li>
+			
+							<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-apartment"></span><span class="mtext"> Leave </span>
+						</a>
+						<ul class="submenu">
+						
+							<li><a href="leaves.php">All Leaves</a></li>
+							<li><a href="pending_leave.php">Pending Leave</a></li>
+							<li><a href="approved_leave.php">Approved Leave</a></li>
+							<li><a href="rejected_leave.php">Rejected Leave</a></li>
+						</ul>
+					</li>
+					
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-apartment"></span><span class="mtext"> My Leave </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="apply_leave.php">Apply Leave</a></li>
+							<li><a href="leave_history.php">Leave history</a></li>
+							</ul>
+					</li>
+					<li>
+						<div class="dropdown-divider"></div>
+					</li>
+					<li>
+						<div class="sidebar-small-cap">Others</div>
+					</li>
+					<li>
+						<a href="https://intranet.cb.amrita.edu/academics/calendar" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-edit-2"></span><span class="mtext">Academic Calendar</span>
+						</a>
+					</li>
+					<li>
+						<a href="https://intranet.cb.amrita.edu/TimeTable/Faculty/index.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-edit-2"></span><span class="mtext">Faculty Time Table</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
---
+++ includes/session.php
@@ -0,0 +1,11 @@
+<?php
+ session_start(); 
+if (!isset($_SESSION['alogin']) || (trim($_SESSION['alogin']) == '')) { ?>
+<script>
+window.location = "../index.php";
+</script>
+<?php
+}
+$session_id=$_SESSION['alogin'];
+$session_depart = $_SESSION['arole'];
+?>
